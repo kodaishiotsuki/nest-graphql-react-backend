8085251ab767e29c0f18f5c7225951f5808c0b75
Kodai <0711dr5@gmail.com>
2024-09-18 17:23:18 +0900

    fix:task作成機能の実装

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -6,13 +6,24 @@ export class TaskService {
   tasks: Task[] = [];
 
   getTasks(): Task[] {
-    const task1 = new Task();
-    task1.id = 1;
-    task1.name = 'Task 1';
-    task1.dueDate = '2021-12-31';
-    task1.status = 'NOT_STARTED';
-    task1.description = 'This is the first task';
-    this.tasks.push(task1);
+    // const task1 = new Task();
+    // task1.id = 1;
+    // task1.name = 'Task 1';
+    // task1.dueDate = '2021-12-31';
+    // task1.status = 'NOT_STARTED';
+    // task1.description = 'This is the first task';
+    // this.tasks.push(task1);
     return this.tasks;
+  }
+
+  createTask(name: string, dueDate: string, description?: string): Task {
+    const newTask = new Task();
+    newTask.id = this.tasks.length + 1;
+    newTask.name = name;
+    newTask.dueDate = dueDate;
+    newTask.status = 'NOT_STARTED';
+    newTask.description = description;
+    this.tasks.push(newTask);
+    return newTask;
   }
 }
